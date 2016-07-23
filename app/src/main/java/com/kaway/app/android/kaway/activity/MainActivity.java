6c35873dd87e4315e0c1a9966c704e413efb5f28
Ino Gamalinda <ino@gamalinda.com>
2016-07-23 18:49:25 +0800

    Implement Map
    Signed-off-by: Ino Gamalinda <ino@gamalinda.com>

--- app/src/main/java/com/kaway/app/android/kaway/activity/MainActivity.java
+++ app/src/main/java/com/kaway/app/android/kaway/activity/MainActivity.java
@@ -1,15 +1,48 @@
 package com.kaway.app.android.kaway.activity;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 import com.kaway.app.android.kaway.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    SupportMapFragment mapFragment;
+    GoogleMap map;
+    OnMapReadyCallback mapReadyCallback;
+
+    LatLng initialLocation = new LatLng(14.5511021, 121.0493267); //Arbitrary for now
+    float initialZoom = 16f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init() {
+        mapFragment = SupportMapFragment.newInstance();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.mapPlaceHolder, mapFragment);
+        ft.commit();
+        mapReadyCallback = new MapReadyCallback();
+        mapFragment.getMapAsync(mapReadyCallback);
+    }
+
+    class MapReadyCallback implements OnMapReadyCallback {
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            map = googleMap;
+
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(initialLocation, initialZoom));
+        }
     }
 }
