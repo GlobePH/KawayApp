e90cf674539519f346d313f8eb005ce208b30154
Ino Gamalinda <ino@gamalinda.com>
2016-07-23 20:19:32 +0800

    Create Jeep model
    Signed-off-by: Ino Gamalinda <ino@gamalinda.com>

---
+++ app/src/main/java/com/kaway/app/android/kaway/model/Jeep.java
@@ -0,0 +1,40 @@
+package com.kaway.app.android.kaway.model;
+
+public class Jeep {
+    long id;
+    String name; //optional
+    Route route;
+    RoutePoint location;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Route getRoute() {
+        return route;
+    }
+
+    public void setRoute(Route route) {
+        this.route = route;
+    }
+
+    public RoutePoint getLocation() {
+        return location;
+    }
+
+    public void setLocation(RoutePoint location) {
+        this.location = location;
+    }
+}
