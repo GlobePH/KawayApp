982c41f22807fadea102177bf03d57fc512d47e1
Ino Gamalinda <ino@gamalinda.com>
2016-07-23 19:18:30 +0800

    Draw mock map items
    Signed-off-by: Ino Gamalinda <ino@gamalinda.com>

--- app/src/main/java/com/kaway/app/android/kaway/activity/MainActivity.java
+++ app/src/main/java/com/kaway/app/android/kaway/activity/MainActivity.java
@@ -10,12 +10,22 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.kaway.app.android.kaway.R;
+import com.kaway.app.android.kaway.data.MockData;
+import com.kaway.app.android.kaway.helper.RouteProcessor;
+import com.kaway.app.android.kaway.model.Route;
+import com.kaway.app.android.kaway.model.RouteStop;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     SupportMapFragment mapFragment;
     GoogleMap map;
     OnMapReadyCallback mapReadyCallback;
+
+    MockData mockData = new MockData();
+    List<Route> routes;
+    List<RouteStop> routeStops;
 
     LatLng initialLocation = new LatLng(14.5511021, 121.0493267); //Arbitrary for now
     float initialZoom = 16f;
@@ -35,6 +45,25 @@ public class MainActivity extends AppCompatActivity {
         ft.commit();
         mapReadyCallback = new MapReadyCallback();
         mapFragment.getMapAsync(mapReadyCallback);
+
+        initializeMapItems();
+    }
+
+    private void initializeMapItems() {
+        routes = mockData.getRoutes();
+        routeStops = mockData.getRouteStops();
+    }
+
+    private void drawLines() {
+        for (Route _route : routes) {
+            map.addPolyline(RouteProcessor.processRoute(_route));
+        }
+    }
+
+    private void drawStops() {
+        for (RouteStop routeStop : routeStops) {
+            map.addMarker(RouteProcessor.processRouteStop(routeStop));
+        }
     }
 
     class MapReadyCallback implements OnMapReadyCallback {
@@ -43,6 +72,9 @@ public class MainActivity extends AppCompatActivity {
             map = googleMap;
 
             map.moveCamera(CameraUpdateFactory.newLatLngZoom(initialLocation, initialZoom));
+
+            drawLines();
+            drawStops();
         }
     }
 }
